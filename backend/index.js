76929e12034a5e7bd6e74cdf9af4226bae923cc4
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-03-11 17:50:03 +0530

    save book endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,39 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+//middleware for parsing request body
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('');
+    console.log(req);
+    return response.status(234).send('Book Management');
+});
+
+//Route to save a new book
+app.post('/books', async (req, res) => {
+
+    try {
+
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({ message : 'Send all required fields.' });
+        }
+        const newBook = {
+            title: req.body.title,
+            author: req.body.author,
+            publishYear: req.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return res.status(201).send(book);
+
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send({ message : error.message });
+    }
+
 });
 
 mongoose.connect(mongoDBURL)
