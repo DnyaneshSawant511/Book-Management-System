da1aebce59ac0ef685fdbedc79860d87c20d238c
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-03-12 00:45:14 +0530

    two routes added

--- backend/index.js
+++ backend/index.js
@@ -49,6 +49,45 @@ app.get('/books', async (req, res) => {
     }
 });
 
+//Route to get one book from the database using id
+app.get('/books/:id', async (req, res) => {
+    try {
+
+        const { id } = req.params;
+
+        const book = await Book.findById(id);
+        return res.status(200).json(book);
+
+    } catch(error){
+        console.log(error.message);
+        res.status(500).send({ message : error.message});
+    }
+});
+
+//Route to update a book
+app.put('/books/:id', async (req, res) => {
+    try {
+
+        if(!req.body.title || !req.body.author || !req.body.publishYear){
+            return res.status(400).send({ message : 'Send all required fields.' });
+        }
+
+        const { id } = req.params;
+
+        const result = await Book.findByIdAndUpdate(id, req.body);
+
+        if(!result){
+            return res.status(404).json({ message : 'Book Not Found' });
+        }
+
+        return res.status(200).send({ message : 'Book Updated Successfully' });
+
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).send({ message : error.message});
+    }
+});
+
 mongoose.connect(mongoDBURL)
 .then(()=>{
     console.log('Connected to database.');
