e3b317ea045c9bd08fe94c7cf198b36f36f2f9c8
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-03-12 18:58:11 +0530

    handled cors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,30 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 //middleware for parsing request body
 app.use(express.json());
+
+//middleware for cors policy
+
+//allow all origins
+app.use(cors());
+
+//allow custom origins
+/*
+
+app.use(
+    cors({
+        origin: 'http://localhost:3000',     # react server
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
+
+*/
 
 app.get('/', (req, res) => {
     console.log(req);
