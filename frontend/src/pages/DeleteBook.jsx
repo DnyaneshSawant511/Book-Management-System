63120bc2d46d066525fa6892e392bf8c3ea191a7
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-03-20 01:24:56 +0530

    delete modified

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -24,16 +24,17 @@ const DeleteBook = () => {
     return (
         <div className="p-4">
             <BackButton />
-            <h1 className="text-3xl my-4">Delete Book</h1>
+            <h1 className="text-3xl my-4">Delete Book 🗑️ </h1>
             {
                 loading ? <Spinner /> : ''
             }
             <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-
-                <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
-                <button className="p-4 bg-red-600 text-white m-8 w-full" onClick={handleDeleteBook}>Yes, Delete it.</button>
-
+                <h3 className="text-2xl font-semibold text-gray-800 mb-8">Are you sure you want to delete this book?</h3>
+                <button className="py-3 px-6 bg-red-600 text-white rounded-md w-full max-w-md hover:bg-red-700 transition duration-300 ease-in-out">
+                    Yes, Delete it
+                </button>
             </div>
+
         </div>
     );
 }
